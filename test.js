947870619881f95440ff996f4cb3b46797715f6e
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2025-01-28 10:32:05 +0100

    kep eltuntetese

--- test.js
+++ test.js
@@ -3,3 +3,8 @@ console.log("Hello World");
 function kep_beilleszt() {
     document.getElementById("kep_helye").innerHTML = "<img src='https://sweetgift.hu/media/catalog/product/cache/e5770af72ca06f4eef4196dd00195758/g/r/green-alma.jpg' width='300px'>"
 }
+
+function kep_torlese() {
+    document.getElementById("kep_helye").innerHTML = ""
+}
+
