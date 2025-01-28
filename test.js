19e1ae04a47c7308557b197885114d951551f492
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2025-01-28 09:34:08 +0100

    kep beilleszt kep torlese

--- test.js
+++ test.js
@@ -1,1 +1,5 @@
 console.log("Hello World");
+
+function kep_beilleszt() {
+    document.getElementById("kep_helye").innerHTML = "<img src='https://sweetgift.hu/media/catalog/product/cache/e5770af72ca06f4eef4196dd00195758/g/r/green-alma.jpg' width='300px'>"
+}
