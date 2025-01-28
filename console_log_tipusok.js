5ee862c578120461b8dc82e84ea6d8fd45833e47
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2025-01-28 10:23:07 +0100

    console log tipusok

---
+++ console_log_tipusok.js
@@ -0,0 +1,10 @@
+console.log("Ez egy normál üzenet.");
+console.warn("Ez egy figyelmeztés.");
+console.error("Ez egy hibaüzenet.");
+
+console.table(
+    [
+        {név: "Anna", kor: 25},
+        {név: "Béla", kor: 30},
+    ]
+);
